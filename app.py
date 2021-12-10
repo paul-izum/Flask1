af8ea44d5e3e7cfa5b141e3a07e8052f064c5815
prudnikovpavel <pavelprudnikov2012@yandex.ru>
2021-12-10 09:39:27 -0800

    add heroku set + add path bd

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 from flask import Flask, jsonify, abort, request
 from sqlite3 import Error
@@ -9,7 +10,8 @@ BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'test.db'}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) \
+                                        or f"sqlite:///{BASE_DIR / 'test.db'}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
